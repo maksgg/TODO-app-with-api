e82476a624df427c766387c31974b2e5f5588e8a
maksgg <maks.zhovmir.97@gmail.com>
2026-01-13 19:23:18 +0200

    feat: added sidebar widget

--- src/widgets/Header/Header.vue
+++ src/widgets/Header/Header.vue
@@ -1,23 +1,13 @@
 <script setup lang="ts">
+import LanguageSelector from "@/features/i18n/components/LanguageSelector.vue";
 import ThemeToggle from "@/features/theme/components/ThemeToggle.vue";
 </script>
 
 <template>
-  <header class="relative w-full min-h-[11vh]">
-    <div
-      class="absolute inset-0 bg-primary
-             [mask-image:linear-gradient(to_bottom,black_30%,transparent_100%)]"
-    />
-    <div
-      class="relative z-10 flex justify-between items-center pl-10 pr-10 gap-5 h-full min-h-[11vh]"
-    >
-      <div class="flex justify-center items-center text-text-color border border-bg-color">
-        Logo
-      </div>
-      <div>
-        <VDropDown />
-        <ThemeToggle />
-      </div>
+  <header class="flex justify-end w-full">
+    <div class="flex items-center gap-10 pr-5 pt-5">
+      <LanguageSelector />
+      <ThemeToggle />
     </div>
   </header>
 </template>
