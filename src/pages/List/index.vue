089b7b540cc9699ed8c792873e1cccb5bee27bd1
maksgg <maks.zhovmir.97@gmail.com>
2026-01-13 19:23:59 +0200

    feat: added pages

---
+++ src/pages/List/index.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import List from "@/features/lists/components/List.vue";
+</script>
+
+<template>
+  <List />
+</template>
+
---
+++ src/pages/Profile/index.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import NewUserProfile from "@/features/profile/NewUserProfile.vue";
+</script>
+
+<template>
+  <NewUserProfile />
+</template>
