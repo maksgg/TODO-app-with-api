adb64f4dc9a563d05b0383aea96f1a876115b468
maksgg <maks.zhovmir.97@gmail.com>
2026-02-16 10:29:29 +0200

    feat: add Header with teleport logic

--- src/widgets/Header/Header.vue
+++ src/widgets/Header/Header.vue
@@ -1,13 +1,72 @@
 <script setup lang="ts">
-import LanguageSelector from "@/features/i18n/components/LanguageSelector.vue";
-import ThemeToggle from "@/features/theme/components/ThemeToggle.vue";
+import { computed } from "vue";
+import { useRoute } from "vue-router";
+
+import { useTasksStore } from "@/features/tasks/store/useTasksStore";
+import { useAuthStore } from "@/shared/stores/useAuthStore";
+import VButton from "@/shared/ui/common/VButton.vue";
+import VSkeleton from "@/shared/ui/common/VSkeleton.vue";
+
+const authStore = useAuthStore();
+const tasksStore = useTasksStore();
+
+const route = useRoute();
+
+const headerLoader = computed(() => tasksStore.loading || authStore.loading);
+const header = computed(() => {
+  if (route.name === "List" && route.query.id) {
+    return { title: tasksStore.allTasks?.listInfo?.title, path: "/list" };
+  }
+  if (route.name === "Dashboard") {
+    return { title: `Good to see you again, ${authStore.userData?.name}` };
+  }
+  if (route.meta.headerTitle) {
+    return { title: route.meta.headerTitle };
+  }
+
+  return { title: "" };
+});
 </script>
 
 <template>
-  <header class="flex justify-end w-full">
-    <div class="flex items-center gap-10 pr-5 pt-5">
-      <LanguageSelector />
-      <ThemeToggle />
+  <div
+    v-show="header.title || headerLoader"
+    class="flex justify-between items-center bg-secondaryBg
+    header-container rounded-xl py-3 px-4"
+  >
+    <VSkeleton
+      v-if="headerLoader && !tasksStore.targetTaskLoader"
+      width="30%"
+      height="30"
+    />
+    <div
+      v-else
+      class="flex items-center gap-2"
+    >
+      <VButton
+        v-if="header.path"
+        icon="chevronLeft"
+        variant="ghost"
+        :show-text="false"
+        :to="header.path"
+        class="text-secondary"
+      />
+      <h2 class="text-headingCard text-txtPrimary truncate">
+        {{ header?.title }}
+      </h2>
     </div>
-  </header>
+
+    <div id="header-action" />
+  </div>
 </template>
+
+<style scoped>
+.header-container {
+  border: 2px solid transparent;
+  background-image:
+    linear-gradient(var(--color-secondaryBg), var(--color-secondaryBg)),
+    linear-gradient(90deg, rgba(62, 13, 168, 1) 0%, rgba(95, 134, 239, 1) 100%);
+  background-origin: border-box;
+  background-clip: padding-box, border-box;
+}
+</style>
